ab0994189d130c370a686a8fc38664ea0f45bd32
svtter <svtter@qq.com>
2023-08-02 11:02:49 +0800

    chore: 2.11 failed with error. cudnn.

--- images/tf2.dockerfile
+++ images/tf2.dockerfile
@@ -1,7 +1,7 @@
 # seems not stable
-# FROM tensorflow/tensorflow:2.13.0-gpu-jupyter
+FROM tensorflow/tensorflow:2.13.0-gpu-jupyter
 
-FROM tensorflow/tensorflow:2.11.0-gpu-jupyter
+# FROM tensorflow/tensorflow:2.11.0-gpu-jupyter
 # my version tensorflow/tensorflow:2.13.0-gpu-jupyter
 
 RUN pip install "requests[socks]" "black[jupyter]"
--- scripts/start_tf2.sh
+++ scripts/start_tf2.sh
@@ -2,5 +2,9 @@
 
 # start tf2 container.
 docker network create --driver bridge tf-net
-docker run --rm -d -p 8889:8888 -v $(pwd)/:/tf/project-transformer --gpus all --network tf-net --name tf-container svtter/tensorflow:2.11.0-gpu-jupyter
+
+# IMAGE_NAME="svtter/tensorflow:2.11.0-gpu-jupyter"
+IMAGE_NAME="svtter/tensorflow:2.13.0-gpu-jupyter"
+
+docker run --rm -d -p 8889:8888 -v $(pwd)/:/tf/project-transformer --gpus all --network tf-net --name tf-container $IMAGE_NAME
 docker run --rm -d -v $(pwd)/config.json:/app/config.json --network tf-net --name tf-v2ray svtter/v2ray-cli:latest
